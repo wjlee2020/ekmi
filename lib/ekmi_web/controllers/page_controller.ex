22a7ad759ea52d1539382dd9d5ef08af633bc55b
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2023-07-10 19:52:48 +0900

    feat: chart js (daily)

--- lib/ekmi_web/controllers/page_controller.ex
+++ lib/ekmi_web/controllers/page_controller.ex
@@ -4,6 +4,11 @@ defmodule EkmiWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    case conn.assigns[:current_user] do
+      nil ->
+        redirect(conn, to: ~p"/users/log_in")
+      _user ->
+        redirect(conn, to: "/budgets")
+    end
   end
 end
