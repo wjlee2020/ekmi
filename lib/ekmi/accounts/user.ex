d08f0a90d4c9e81a7dffb9c4eddf58908c770253
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2023-07-17 13:25:17 +0900

    fix: user registration changeset

--- lib/ekmi/accounts/user.ex
+++ lib/ekmi/accounts/user.ex
@@ -40,7 +40,7 @@ defmodule Ekmi.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :name])
     |> validate_email(opts)
     |> validate_password(opts)
   end
