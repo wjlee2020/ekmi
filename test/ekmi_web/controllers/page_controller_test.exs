e8a240699b4a5b33a3e17e86c985cd096908fdfb
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2023-10-15 08:06:12 +0900

    test: fix sample page controller test (redirect)

--- test/ekmi_web/controllers/page_controller_test.exs
+++ test/ekmi_web/controllers/page_controller_test.exs
@@ -3,6 +3,8 @@ defmodule EkmiWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+
+    assert html_response(conn, 302) =~
+             "<html><body>You are being <a href=\"/users/log_in\">redirected</a>.</body></html>"
   end
 end
