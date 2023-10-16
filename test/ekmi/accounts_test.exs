1af586fee59631ee4b16e4733e4bdfc77b2a734e
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2023-10-17 08:17:20 +0900

    test: fix accounts tests

--- test/ekmi/accounts_test.exs
+++ test/ekmi/accounts_test.exs
@@ -12,8 +12,8 @@ defmodule Ekmi.AccountsTest do
     end
 
     test "returns the user if the email exists" do
-      %{id: id} = user = user_fixture()
-      assert %User{id: ^id} = Accounts.get_user_by_email(user.email)
+      %{user: %{id: id}} = user = user_fixture()
+      assert %User{id: ^id} = Accounts.get_user_by_email(user.user.email)
     end
   end
 
@@ -23,15 +23,15 @@ defmodule Ekmi.AccountsTest do
     end
 
     test "does not return the user if the password is not valid" do
-      user = user_fixture()
+      %{user: user} = user_fixture()
       refute Accounts.get_user_by_email_and_password(user.email, "invalid")
     end
 
     test "returns the user if the email and password are valid" do
-      %{id: id} = user = user_fixture()
+      %{user: %{id: id}} = user = user_fixture()
 
       assert %User{id: ^id} =
-               Accounts.get_user_by_email_and_password(user.email, valid_user_password())
+               Accounts.get_user_by_email_and_password(user.user.email, valid_user_password())
     end
   end
 
@@ -43,8 +43,8 @@ defmodule Ekmi.AccountsTest do
     end
 
     test "returns the user with the given id" do
-      %{id: id} = user = user_fixture()
-      assert %User{id: ^id} = Accounts.get_user!(user.id)
+      %{user: %{id: id}} = user_fixture()
+      assert %User{id: ^id} = Accounts.get_user!(id)
     end
   end
 
@@ -59,7 +59,8 @@ defmodule Ekmi.AccountsTest do
     end
 
     test "validates email and password when given" do
-      {:error, changeset} = Accounts.register_user(%{email: "not valid", password: "not valid"})
+      {:error, changeset} =
+        Accounts.register_user(%{email: "not valid", password: "not valid"})
 
       assert %{
                email: ["must have the @ sign and no spaces"],
@@ -75,8 +76,8 @@ defmodule Ekmi.AccountsTest do
     end
 
     test "validates email uniqueness" do
-      %{email: email} = user_fixture()
-      {:error, changeset} = Accounts.register_user(%{email: email})
+      %{user: %{email: email}} = user_fixture()
+      {:error, :user, changeset} = Accounts.register_user(%{email: email})
       assert "has already been taken" in errors_on(changeset).email
 
       # Now try with the upper cased email too, to check that email case is ignored.
@@ -86,7 +87,7 @@ defmodule Ekmi.AccountsTest do
 
     test "registers users with a hashed password" do
       email = unique_user_email()
-      {:ok, user} = Accounts.register_user(valid_user_attributes(email: email))
+      {:ok, %{user: user}} = Accounts.register_user(valid_user_attributes(email: email))
       assert user.email == email
       assert is_binary(user.hashed_password)
       assert is_nil(user.confirmed_at)
@@ -129,19 +130,19 @@ defmodule Ekmi.AccountsTest do
       %{user: user_fixture()}
     end
 
-    test "requires email to change", %{user: user} do
+    test "requires email to change", %{user: %{user: user}} do
       {:error, changeset} = Accounts.apply_user_email(user, valid_user_password(), %{})
       assert %{email: ["did not change"]} = errors_on(changeset)
     end
 
-    test "validates email", %{user: user} do
+    test "validates email", %{user: %{user: user}} do
       {:error, changeset} =
         Accounts.apply_user_email(user, valid_user_password(), %{email: "not valid"})
 
       assert %{email: ["must have the @ sign and no spaces"]} = errors_on(changeset)
     end
 
-    test "validates maximum value for email for security", %{user: user} do
+    test "validates maximum value for email for security", %{user: %{user: user}} do
       too_long = String.duplicate("db", 100)
 
       {:error, changeset} =
@@ -150,8 +151,8 @@ defmodule Ekmi.AccountsTest do
       assert "should be at most 160 character(s)" in errors_on(changeset).email
     end
 
-    test "validates email uniqueness", %{user: user} do
-      %{email: email} = user_fixture()
+    test "validates email uniqueness", %{user: %{user: user}} do
+      %{user: %{email: email}} = user_fixture()
       password = valid_user_password()
 
       {:error, changeset} = Accounts.apply_user_email(user, password, %{email: email})
@@ -159,14 +160,14 @@ defmodule Ekmi.AccountsTest do
       assert "has already been taken" in errors_on(changeset).email
     end
 
-    test "validates current password", %{user: user} do
+    test "validates current password", %{user: %{user: user}} do
       {:error, changeset} =
         Accounts.apply_user_email(user, "invalid", %{email: unique_user_email()})
 
       assert %{current_password: ["is not valid"]} = errors_on(changeset)
     end
 
-    test "applies the email without persisting it", %{user: user} do
+    test "applies the email without persisting it", %{user: %{user: user}} do
       email = unique_user_email()
       {:ok, user} = Accounts.apply_user_email(user, valid_user_password(), %{email: email})
       assert user.email == email
@@ -179,7 +180,7 @@ defmodule Ekmi.AccountsTest do
       %{user: user_fixture()}
     end
 
-    test "sends token through notification", %{user: user} do
+    test "sends token through notification", %{user: %{user: user}} do
       token =
         extract_user_token(fn url ->
           Accounts.deliver_user_update_email_instructions(user, "current@example.com", url)
@@ -195,7 +196,7 @@ defmodule Ekmi.AccountsTest do
 
   describe "update_user_email/2" do
     setup do
-      user = user_fixture()
+      %{user: user} = user_fixture()
       email = unique_user_email()
 
       token =
@@ -222,7 +223,10 @@ defmodule Ekmi.AccountsTest do
       assert Repo.get_by(UserToken, user_id: user.id)
     end
 
-    test "does not update email if user email changed", %{user: user, token: token} do
+    test "does not update email if user email changed", %{
+      user: %{user: user},
+      token: token
+    } do
       assert Accounts.update_user_email(%{user | email: "current@example.com"}, token) == :error
       assert Repo.get!(User, user.id).email == user.email
       assert Repo.get_by(UserToken, user_id: user.id)
@@ -272,7 +276,7 @@ defmodule Ekmi.AccountsTest do
              } = errors_on(changeset)
     end
 
-    test "validates maximum values for password for security", %{user: user} do
+    test "validates maximum values for password for security", %{user: %{user: user}} do
       too_long = String.duplicate("db", 100)
 
       {:error, changeset} =
@@ -281,14 +285,14 @@ defmodule Ekmi.AccountsTest do
       assert "should be at most 72 character(s)" in errors_on(changeset).password
     end
 
-    test "validates current password", %{user: user} do
+    test "validates current password", %{user: %{user: user}} do
       {:error, changeset} =
         Accounts.update_user_password(user, "invalid", %{password: valid_user_password()})
 
       assert %{current_password: ["is not valid"]} = errors_on(changeset)
     end
 
-    test "updates the password", %{user: user} do
+    test "updates the password", %{user: %{user: user}} do
       {:ok, user} =
         Accounts.update_user_password(user, valid_user_password(), %{
           password: "new valid password"
@@ -298,7 +302,7 @@ defmodule Ekmi.AccountsTest do
       assert Accounts.get_user_by_email_and_password(user.email, "new valid password")
     end
 
-    test "deletes all tokens for the given user", %{user: user} do
+    test "deletes all tokens for the given user", %{user: %{user: user}} do
       _ = Accounts.generate_user_session_token(user)
 
       {:ok, _} =
@@ -315,7 +319,7 @@ defmodule Ekmi.AccountsTest do
       %{user: user_fixture()}
     end
 
-    test "generates a token", %{user: user} do
+    test "generates a token", %{user: %{user: user}} do
       token = Accounts.generate_user_session_token(user)
       assert user_token = Repo.get_by(UserToken, token: token)
       assert user_token.context == "session"
@@ -324,7 +328,7 @@ defmodule Ekmi.AccountsTest do
       assert_raise Ecto.ConstraintError, fn ->
         Repo.insert!(%UserToken{
           token: user_token.token,
-          user_id: user_fixture().id,
+          user_id: user_fixture().user.id,
           context: "session"
         })
       end
@@ -333,7 +337,7 @@ defmodule Ekmi.AccountsTest do
 
   describe "get_user_by_session_token/1" do
     setup do
-      user = user_fixture()
+      %{user: user} = user_fixture()
       token = Accounts.generate_user_session_token(user)
       %{user: user, token: token}
     end
@@ -355,7 +359,7 @@ defmodule Ekmi.AccountsTest do
 
   describe "delete_user_session_token/1" do
     test "deletes the token" do
-      user = user_fixture()
+      %{user: user} = user_fixture()
       token = Accounts.generate_user_session_token(user)
       assert Accounts.delete_user_session_token(token) == :ok
       refute Accounts.get_user_by_session_token(token)
@@ -367,7 +371,7 @@ defmodule Ekmi.AccountsTest do
       %{user: user_fixture()}
     end
 
-    test "sends token through notification", %{user: user} do
+    test "sends token through notification", %{user: %{user: user}} do
       token =
         extract_user_token(fn url ->
           Accounts.deliver_user_confirmation_instructions(user, url)
@@ -383,7 +387,7 @@ defmodule Ekmi.AccountsTest do
 
   describe "confirm_user/1" do
     setup do
-      user = user_fixture()
+      %{user: user} = user_fixture()
 
       token =
         extract_user_token(fn url ->
@@ -407,7 +411,7 @@ defmodule Ekmi.AccountsTest do
       assert Repo.get_by(UserToken, user_id: user.id)
     end
 
-    test "does not confirm email if token expired", %{user: user, token: token} do
+    test "does not confirm email if token expired", %{user: %{user: user}, token: token} do
       {1, nil} = Repo.update_all(UserToken, set: [inserted_at: ~N[2020-01-01 00:00:00]])
       assert Accounts.confirm_user(token) == :error
       refute Repo.get!(User, user.id).confirmed_at
@@ -420,7 +424,7 @@ defmodule Ekmi.AccountsTest do
       %{user: user_fixture()}
     end
 
-    test "sends token through notification", %{user: user} do
+    test "sends token through notification", %{user: %{user: user}} do
       token =
         extract_user_token(fn url ->
           Accounts.deliver_user_reset_password_instructions(user, url)
@@ -436,7 +440,7 @@ defmodule Ekmi.AccountsTest do
 
   describe "get_user_by_reset_password_token/1" do
     setup do
-      user = user_fixture()
+      %{user: user} = user_fixture()
 
       token =
         extract_user_token(fn url ->
@@ -452,7 +456,7 @@ defmodule Ekmi.AccountsTest do
     end
 
     test "does not return the user with invalid token", %{user: user} do
-      refute Accounts.get_user_by_reset_password_token("oops")
+      assert Accounts.get_user_by_reset_password_token("oops") == nil
       assert Repo.get_by(UserToken, user_id: user.id)
     end
 
@@ -468,7 +472,7 @@ defmodule Ekmi.AccountsTest do
       %{user: user_fixture()}
     end
 
-    test "validates password", %{user: user} do
+    test "validates password", %{user: %{user: user}} do
       {:error, changeset} =
         Accounts.reset_user_password(user, %{
           password: "not valid",
@@ -476,24 +480,23 @@ defmodule Ekmi.AccountsTest do
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
 
-    test "validates maximum values for password for security", %{user: user} do
+    test "validates maximum values for password for security", %{user: %{user: user}} do
       too_long = String.duplicate("db", 100)
       {:error, changeset} = Accounts.reset_user_password(user, %{password: too_long})
       assert "should be at most 72 character(s)" in errors_on(changeset).password
     end
 
-    test "updates the password", %{user: user} do
+    test "updates the password", %{user: %{user: user}} do
       {:ok, updated_user} = Accounts.reset_user_password(user, %{password: "new valid password"})
       assert is_nil(updated_user.password)
       assert Accounts.get_user_by_email_and_password(user.email, "new valid password")
     end
 
-    test "deletes all tokens for the given user", %{user: user} do
+    test "deletes all tokens for the given user", %{user: %{user: user}} do
       _ = Accounts.generate_user_session_token(user)
       {:ok, _} = Accounts.reset_user_password(user, %{password: "new valid password"})
       refute Repo.get_by(UserToken, user_id: user.id)
